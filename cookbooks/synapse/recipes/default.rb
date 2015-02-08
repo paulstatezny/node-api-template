b6d2e90cdeffc6c44b721c60942c9d7925a38843
Aaron Bruce <aaron@synapsestudios.com>
2015-02-08 12:04:26 -0700

    using nodemon to restart server when files change

--- cookbooks/synapse/recipes/default.rb
+++ cookbooks/synapse/recipes/default.rb
@@ -5,5 +5,5 @@ end
 
 execute "start-node-app" do
   cwd '/vagrant'
-  command "forever start -o /vagrant/out.log -e /vagrant/error.log server.js"
+  command "forever start -o /vagrant/out.log -e /vagrant/error.log -c nodemon server.js"
 end
--- cookbooks/synapse/recipes/packages.rb
+++ cookbooks/synapse/recipes/packages.rb
@@ -1,3 +1,7 @@
 execute 'sudo npm install -g forever@0.14.1' do
     not_if 'which forever'
 end
+
+execute 'sudo npm install -g nodemon@1.3.6' do
+    not_if 'which nodemon'
+end
