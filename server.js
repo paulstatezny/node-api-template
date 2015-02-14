9d762b4ff2b2d6351c1b48022539a2420df21236
Aaron Bruce <aaron@synapsestudios.com>
2015-02-14 15:56:15 -0700

    including routes in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: 80 });
 
-require('./src/services')(server);
+require('./src/routes')(server);
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
